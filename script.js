fc4e3ccb80317397c273ac7f6b9b1a4ffba895cc
Samuel Hovie <samhovie@umich.edu>
2021-06-30 10:55:33 -0400

    cleanup

--- script.js
+++ script.js
@@ -13,38 +13,6 @@ for (const button of buttons) {
   });
 }
 
-
-// let scene, camera, renderer;
-// function init () {
-//     scene = new THREE.Scene();
-//     scene.background = new THREE.Color(0xdddddd);
-//     camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
-
-//     camera = new THREE.PerspectiveCamera(40,window.innerWidth/window.innerHeight,1,5000);
-//     camera.rotation.y = 45/180*Math.PI;
-//     camera.position.x = 800;
-//     camera.position.y = 100;
-//     camera.position.z = 1000;
-
-//     hlight = new THREE.AmbientLight (0x404040,100);
-//     scene.add(hlight);
-//     renderer = new THREE.WebGLRenderer({antial:true});
-
-//     renderer.setSize(window.innerWidth,window.innerHeight);
-//     document.body.appendChild(renderer.domElement);
-
-//     let loader = new THREE.GLTFLoader();
-
-//     loader.load('scene.gltf', function(gltf) {
-//         car = gltf.scene.children[0];
-//         car.scale.set(0.5,0.5,0.5);
-//         scene.add(gltf.scene);
-//         renderer.render(scene,camera);
-//     });
-// }
-// // init();
-// import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
-
 let scene, camera, renderer;
 
 function init() {
@@ -57,13 +25,6 @@ function init() {
     camera.position.x = 1000;
     camera.position.y = 200;
     camera.position.z = 1000;
-
-    // controls = new THREE.OrbitControls(camera);
-    // controls.addEventListener('change', renderer);
-
-    // let controls = new THREE.OrbitControls(camera);
-    // let controls = new THREE.OrbitControls(camera, renderer.domElement);
-    // controls.addEventListener('change', renderer);
 
     let hlight = new THREE.AmbientLight (0x404040,100);
     scene.add(hlight);
